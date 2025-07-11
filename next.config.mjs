03fce214d5a8a98b8056cf200b1fa5474e061af1
PhucMaii <maithienphuc0102@gmail.com>
2025-07-11 12:42:11 -0700

    add index to all pages

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,10 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin',
           },
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
         ],
       },
     ];
